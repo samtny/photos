d78460ce0b2d01c981c816dc66f13db2bdf10ebc
Sam T <--global>
2014-10-13 22:24:16 -0400

    folder children

--- js/app/ViewModel/FolderViewModel.js
+++ js/app/ViewModel/FolderViewModel.js
@@ -1,10 +1,29 @@
-define(['knockout'], function (ko) {
-  return function folderViewModel(data) {
+define(['knockout', 'underscore'], function (ko) {
+  var folderViewModel = function (data, nestlevel) {
     var folder = this;
 
     folder.name = ko.observable(data.name);
-    folder.parent = ko.observable(data.parent);
+    folder.nestlevel = nestlevel || 0;
+
+    folder.parents = ko.observableArray([]);
+    folder.children = ko.observableArray([]);
+
+    _.each(data.parents, function (item) {
+      folder.parents.push(new folderViewModel(item))
+    });
+
+    _.each(data.children, function (item) {
+      folder.children.push(new folderViewModel(item, nestlevel + 1));
+    });
+
+    folder.expanded = ko.observable(false);
 
     console.log('folder', folder);
   };
+
+  folderViewModel.prototype.toggleExpanded = function () {
+    this.expanded(!this.expanded());
+  };
+
+  return folderViewModel;
 });
